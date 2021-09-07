4be5307082ff8e1992278f440bcfb3fc3b703f4e
Maryann Mwikali <maryann.mwikali@student.moringaschool.com>
2021-09-07 20:32:02 +0300

    correct code on js file

--- script.js
+++ script.js
@@ -1,29 +1,33 @@
 function generateName(){
     var date = document.getElementById('date').value;
-    
     var month = document.getElementById('month').value;
-   
-    var yearOfBirth = document.getElementById('year').value;
+    var year = document.getElementById('year').value;
+    var birthDay= new Date(year+ '/' +month + '/' + date);
+    var dayOfWeek=birthDay.getDay();
     
-  
-    var century = yearOfBirth.slice(0,2);
-    var year = yearOfBirth.slice(2,4);
-  
     var genders = document.getElementById("gender");
     var gender = genders.options[genders.selectedIndex].text;
-  
+
+
     var maleName = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
     var femaleName = ["Akosu", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Amna"];
-    var dayOfWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-  
-    var dayOfBirth = ( ( (century/4) -2*year-1) + ((5*yearOfBirth/4) ) + ((26*(month+1)/10)) + date ) % 7;
-    var dayOfBirth = Math.floor(dayOfBirth);
-  
-    if (gender === "Male") {
-         document.getElementById("result").innerHTML = "You were born on " + dayOfWeek[dayOfBirth - 1]+ " And your name is " +  maleName[dayOfBirth -1];
-         }
-         else if (gender === "Female") {
-             document.getElementById("result").innerHTML = "You were born on " + dayOfWeek[dayOfBirth - 1]+ " And your name is " +  femaleName[dayOfBirth -1];
-         }
-      
-  }
+    var days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    
+
+    if (date <=0 && date>31){
+        document.getElementById("result").innerHTML="Please enter a valid date!"
+    }
+    else if(month<0 && month>12){
+        document.getElementById("result").innerHTML="Please enter a valid month!"
+    }
+    else if((gender === "Female") && (gender === "Male")){
+        document.getElementById("result").innerHTML="Please select gender!"
+    }
+    else if (gender === "Male"){
+        document.getElementById("result").innerHTML="You were born on a" + days[dayOfWeek] +  "your Akan name is" + maleName[dayOfWeek]
+    }
+    else if (gender === "Female"){
+        document.getElementById("result").innerHTML="You were born on a" + days[dayOfWeek] +  "your Akan name is" + femaleName[dayOfWeek]
+    }
+    else{document.getElementById("result").innerHTML="Please check your input!"}
+ }
